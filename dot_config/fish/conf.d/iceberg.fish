33272f16dbcf254f3dc5e729dc232ca2ee29595b
Shota Kubo <4653726+syou910@users.noreply.github.com>
2026-08-22 21:07:17 +0900

    Add Iceberg color scheme for pure prompt

---
+++ dot_config/fish/conf.d/iceberg.fish
@@ -0,0 +1,9 @@
+# Iceberg テーマに合わせた pure プロンプトの配色
+set --global pure_color_primary 84a0c6 # blue
+set --global pure_color_info 89b8c2 # cyan
+set --global pure_color_mute 6b7089 # brblack
+set --global pure_color_success ada0d3 # magenta
+set --global pure_color_danger e27878 # red
+set --global pure_color_light d2d4de # white
+set --global pure_color_warning e2a478 # yellow
+set --global pure_color_dark 161821 # black
